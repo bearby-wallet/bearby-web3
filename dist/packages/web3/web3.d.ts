ea51a70f1f872b6e118dff5f842f6abc0613a9c8
Rinat <lich666black@gmail.com>
2022-10-07 18:22:53 +0300

    pump verions

--- dist/packages/web3/web3.d.ts
+++ dist/packages/web3/web3.d.ts
@@ -1,10 +1,9 @@
 import { Contract } from './contract';
-import { ContentProvider, Massa } from './massa';
+import { Massa } from './massa';
 import { Wallet } from './wallet';
 export declare class Web3 {
     #private;
     readonly wallet: Wallet;
-    readonly provider: ContentProvider;
     readonly contract: Contract;
     readonly massa: Massa;
     constructor();
