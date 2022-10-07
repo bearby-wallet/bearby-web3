4a9c0ec679350112306d862769b99e0c43bf1300
Rinat <lich666black@gmail.com>
2022-10-07 18:21:22 +0300

    fixed provider

--- packages/web3/web3.ts
+++ packages/web3/web3.ts
@@ -6,16 +6,16 @@ import { Wallet  } from './wallet';
 
 export class Web3 {
   readonly #handler = new Handler();
+  readonly #provider = new ContentProvider(
+    this.#handler.stream,
+    this.#handler.subject
+  );
   readonly wallet = new Wallet(
     this.#handler.stream,
     this.#handler.subject
   );
-  readonly provider = new ContentProvider(
-    this.#handler.stream,
-    this.#handler.subject
-  );
-  readonly contract = new Contract(this.provider);
-  readonly massa = new Massa(this.provider, this.wallet);
+  readonly contract = new Contract(this.#provider);
+  readonly massa = new Massa(this.#provider, this.wallet);
 
   constructor() {
     this.#handler.initialized();
