5def5ada2fbab57c87f94d43d713d5e4d999a2f8
Hicaru <hicc0rus@gmail.com>
2022-10-10 16:46:03 +0300

    added defualt export

--- dist/lib/errors.d.ts
+++ dist/lib/errors.d.ts
@@ -3,3 +3,4 @@ export declare const WALLET_IS_NOT_CONNECTED = "Wallet is not connected";
 export declare const INVALID_SIGN_PARAMS = "Invalid sign params";
 export declare const TIME_OUT = "Request failed by timeout";
 export declare const AVAILABLE_ONLY_BROWSER = "bearby-web3 available only browser";
+export declare const WEB3_INSTANCE_CREATED = "bearby Web3 instance already created!";
