dad8257a74bf622b94beb9d94f68a149c9545133
Hicaru <hicc0rus@gmail.com>
2022-10-24 14:04:37 +0300

    fixed type

--- index.ts
+++ index.ts
@@ -4,5 +4,6 @@ export * from './packages';
 export * from './packages/web3';
 export * from './packages/wallet';
 export * from './packages/massa';
+export * from './packages/contract';
 
 export default web3;
