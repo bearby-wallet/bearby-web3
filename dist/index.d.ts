f14297e30a028cde43c177591e9af874c3d459b4
Hicaru <hicc0rus@gmail.com>
2022-10-24 14:05:08 +0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,5 @@ export * from './packages';
 export * from './packages/web3';
 export * from './packages/wallet';
 export * from './packages/massa';
+export * from './packages/contract';
 export default web3;
