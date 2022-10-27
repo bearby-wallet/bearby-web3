4015e6e11dce4521846a996465a34bc8f89ac590
Hicaru <hicc0rus@gmail.com>
2022-10-27 18:53:01 +0300

    pump version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,7 @@
+import { web3 } from './packages';
 export * from './packages';
 export * from './packages/contract';
 export * from './packages/massa';
 export * from './packages/wallet';
 export * from './packages/web3';
+export default web3;
